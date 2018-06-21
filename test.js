43dbf523517c3ebc993fb0f55b62b90af1f9ef2c
Cannon Collins <cannon.j.collins@gmail.com>
2018-06-21 08:55:00 -0600

    small changes not working

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ async function run() {
 	const pageToClick = 'body > table > tbody > tr:nth-child(3) > td > table > tbody > tr > td > form > table > tbody > tr:nth-child(3) > td > div > input[type="submit"]';
 	const select = 'body > table > tbody > tr:nth-child(3) > td > table > tbody > tr > td > form > table > tbody > tr:nth-child(1) > td:nth-child(2) > select';
 	const inputField = 'body > table > tbody > tr:nth-child(3) > td > table > tbody > tr > td > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > input[type="text"]:nth-child(1)';
-	const links = 'body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table.bodytext > tbody a';
+	// const links = 'body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table.bodytext > tbody a';
 	const browser = await puppeteer.launch({
 		headless: false
 	});
@@ -17,11 +17,19 @@ async function run() {
 		page.waitForNavigation(),
 		page.click(pageToClick)
 	]);
-	const records = await page.$x('//table[2]//tr[td[a]]');
-	for (record in records) {
-		console.log(record);
+	const links = await page.evaluate(() => {
+		const records = document.evaluate('//table[2]//tr[td[a]]', document, null, XPathResult.ANY_TYPE, null);
+		// records = document.$x('//table[2]//tr[td[a]]');
+		return records
+		// return records.map(record => record.getElementsByTagName('td')[0].children[0]);
+	})
+	console.log("--------");
+	console.log(links.length);
+	console.log("--------");
+	for (let linc in links) {
+		console.log(linc);
 	}
-	console.log(records.length);
+	console.log(links.length);
 	const pic = await page.screenshot({fullPage: true});
 	await page.close();
 	await browser.close();
